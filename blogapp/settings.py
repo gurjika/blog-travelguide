677deb229e69c49242b5c7104bd2039ebb604e31
Luka <lgurjidze@gmail.com>
2024-08-02 14:36:48 +0400

    dockerize app with mysql

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -22,7 +22,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_zdvt98@+0$-b1ujd^y@$afx^%iz6q3%5z^cv+dmjh*!y04pn^'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
