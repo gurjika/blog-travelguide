aeb01b8dff878418292b5d8cfecd742b6c2e0744
Luka <lgurjidze@gmail.com>
2024-08-02 14:14:48 +0400

    create models

--- blog_api/models.py
+++ blog_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField('core.user', related_name='profile', on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(upload_to='profile_pics', null=True)
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(Profile, related_name='blogs', on_delete=models.CASCADE)
+    
+
+class Comment(models.Model):
+    content = models.TextField()
+    author = models.ForeignKey(Profile, related_name='comments', on_delete=models.CASCADE)
+    blogpost = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='blog_comments')
+
+    
--- blogapp/urls.py
+++ blogapp/urls.py
@@ -20,4 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path("__debug__/", include("debug_toolbar.urls")),
     path('admin/', admin.site.urls),
+    path('api/', include('blog_api.urls')),
+    path('auth/', include('core.urls'))
 ]
