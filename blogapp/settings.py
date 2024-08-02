c3ba9a817449cb3cb4093993abb052eb0aa6a5c3
Luka <lgurjidze@gmail.com>
2024-08-02 23:48:20 +0400

    add swagger

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'rest_framework',
     'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -164,6 +165,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 
 }
 
